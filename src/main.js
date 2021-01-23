13a5aed91e19742c50e4e8c4bba1597485065196
Ammar Mughal <meet.sagi.here@gmail.com>
2021-01-23 22:17:43 +0500

    initial setup tailwindcss, aos, anime.js added

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue';
+import aos from 'aos';
+import App from './App.vue';
+import router from './router';
+import './assets/tailwind.css';
+
+createApp(App)
+	.use(aos.init())
+	.use(router)
+	.mount('#app');
